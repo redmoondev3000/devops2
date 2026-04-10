6d92ba02c1dc5965af52fb0c09f8f3e0fb9bf4cd
redmoondev3000 <redmoon.dev3000@gmail.com>
2026-04-10 12:45:18 +0900

    fix: temporary remove backend for clean sync

--- backend/src/main/java/org/cloud/mapper/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package org.cloud.mapper;
-
-import org.cloud.dto.MemberDTO;
-
-public interface MemberMapper {
-	MemberDTO memberLoginCheck(MemberDTO member);
-	void memberJoin(MemberDTO member);
-	int idCheck(String userId);
-}
--- backend/src/main/java/org/cloud/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package org.cloud.service;
-
-import org.cloud.dto.MemberDTO;
-
-public interface MemberService {
-	MemberDTO memberLoginCheck(MemberDTO member);
-	void memberJoin(MemberDTO member);
-	int idCheck(String userId);
-}
